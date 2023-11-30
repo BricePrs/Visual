1df6d7eac14a15e9ee082ea09bc9be3b50903444
Brice <brice.peres@gmail.com>
2023-11-30 22:38:26 +0100

    Smol update

---
+++ include/Collider.h
@@ -0,0 +1,18 @@
+//
+// Created by brice on 11/29/23.
+//
+
+#ifndef VISUAL_COLLIDER_H
+#define VISUAL_COLLIDER_H
+
+#include <glm/glm.hpp>
+
+class Collider {
+public:
+    [[nodiscard]] virtual bool Intersect(glm::vec3 pt) const = 0;
+    [[nodiscard]] virtual bool Intersect(glm::vec3 LowerAABB, glm::vec3 UpperAABB) const = 0;
+    [[nodiscard]] virtual glm::vec3 ShortestSurfacePoint(glm::vec3 pt) const = 0;
+};
+
+
+#endif //VISUAL_COLLIDER_H
